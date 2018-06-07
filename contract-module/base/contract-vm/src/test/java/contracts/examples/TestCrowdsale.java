e684358b8c48f08d764b534518756070fd95d97d
xieno1 <xdehuan@163.com>
2018-06-07 11:13:29 +0800

    TestCrowdsale

--- contract-module/base/contract-vm/src/test/java/contracts/examples/TestCrowdsale.java
+++ contract-module/base/contract-vm/src/test/java/contracts/examples/TestCrowdsale.java
@@ -1,14 +1,17 @@
 package contracts.examples;
 
-import contracts.crowdsale.Crowdsale;
+import contracts.crowdsale.emission.MintedCrowdsale;
 import io.nuls.contract.sdk.Address;
 
 import java.math.BigInteger;
 
-public class TestCrowdsale extends Crowdsale {
+import static io.nuls.contract.sdk.Utils.require;
 
-    public TestCrowdsale(BigInteger rate, Address wallet, Address token) {
-        super(rate, wallet, token);
+public class TestCrowdsale extends MintedCrowdsale {
+
+    public TestCrowdsale(long openingTime, long closingTime, BigInteger rate, Address wallet, BigInteger cap, Address token, BigInteger goal) {
+        super(openingTime, closingTime, rate, wallet, cap, token, goal);
+        require(goal.compareTo(cap) <= 0);
     }
 
 }
